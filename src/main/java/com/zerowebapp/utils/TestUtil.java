c440232fe24571f24c99acef24c0c9b9d47e9dfb
Edwin Ndonga <ndonga.edwin@gmail.com>
2020-09-20 02:29:56 +0300

    added testng.xml test runner file

--- src/main/java/com/zerowebapp/utils/TestUtil.java
+++ src/main/java/com/zerowebapp/utils/TestUtil.java
@@ -1,6 +1,12 @@
 package com.zerowebapp.utils;
 
-public class TestUtil {
+import com.zerowebapp.base.TestBase;
+
+public class TestUtil extends TestBase {
     public static long PAGE_LOAD_TIMEOUT = 5;
     public static long IMPLICIT_WAIT = 3;
+
+    public void switchToFrame(String frameName){
+        driver.switchTo().frame(frameName);
+    }
 }
